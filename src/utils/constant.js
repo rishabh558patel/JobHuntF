d06c3b978732a8fda388cccb79a9d27d77ec3124
Rishabh Patel <153888559+rishabh558patel@users.noreply.github.com>
2025-08-07 22:27:16 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,5 @@
-export const VITE_API_URL="https://job-hunt-b.vercel.app"
-export const USER_API_END_POINT = "https://job-hunt-b.vercel.app/api/v1/user";
-export const JOB_API_END_POINT = "https://job-hunt-b.vercel.app/api/v1/job";
-export const APPLICATION_API_END_POINT = "https://job-hunt-b.vercel.app/api/v1/application";
-export const COMPANY_API_END_POINT = "https://job-hunt-b.vercel.app/api/v1/company";
+export const VITE_API_URL="https://jobhuntb.onrender.com"
+export const USER_API_END_POINT = `${VITE_API_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${VITE_API_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${VITE_API_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${VITE_API_URL}/api/v1/company`;
